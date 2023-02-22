6715eae27ab93215e4a35f51ebac474d751b3172
vdps <vdps007@gmail.com>
2023-02-22 13:14:43 +0530

    update

--- loops.sh
+++ loops.sh
@@ -1,11 +1,18 @@
 read -p 'enter the last number: ' num1
 echo "your last number is = ${num1}"
- 
-for (( i=1; i<=${num1}; i++ )) do
-    echo $i
-    sleep 1
-    if [ $i == ${num1} ]; then
-        echo 'end of the last number'
+b=2
+
+while [ $b -le 2 ]
+do
+
+
+    for (( i=1; i<=${num1}; i++ )) do
+        echo $i
         sleep 1
-    fi
+        if [ $i == ${num1} ]; then
+            echo 'end of the last number'
+            sleep 1
+        fi
+    done
+    b=$(($b+1))
 done
